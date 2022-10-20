ea9d9a33701ab5551718f908f2efec49be0dce21
cateldev <catelmatt@hotmail.com>
2022-10-20 15:34:47 -0300

    Projeto Bytebank - Criando metodos

--- Csharp/Csharp/ContaCorrente.cs
+++ Csharp/Csharp/ContaCorrente.cs
@@ -7,5 +7,30 @@ namespace Csharp
         public string? conta;
         public int agencia;
         public double saldo;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Transferir(double valor, ContaCorrente destinatario)
+        {
+           this.Sacar(valor);
+            destinatario.Depositar(valor);
+        }
     }
 }
